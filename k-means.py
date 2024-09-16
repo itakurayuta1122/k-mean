575d2fa9ea3aaccf8d6a030c79b015a16d3d3424
itakurayuta1122 <81971399+itakurayuta1122@users.noreply.github.com>
2024-09-17 01:04:42 +0900

    Add files via upload

--- k-means.py
+++ k-means.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 dataset = pd.read_csv("k-means.csv")
-
+dataset = dataset.drop('Unnamed:0', axis=1)
 st.write("クラスタリング前",dataset)
 
 data = dataset.sample(frac=0.95, random_state=786).reset_index(drop=True)
